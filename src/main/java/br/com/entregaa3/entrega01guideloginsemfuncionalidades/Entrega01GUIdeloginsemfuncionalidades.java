0d116b83e8a405025fb11b7f683e6d17696fe8b5
Henryk18 <henrykbagda@gmail.com>
2023-10-05 20:49:53 -0300

    Entrega01 GUI login sem funcionalidades

---
+++ src/main/java/br/com/entregaa3/entrega01guideloginsemfuncionalidades/Entrega01GUIdeloginsemfuncionalidades.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.entregaa3.entrega01guideloginsemfuncionalidades;
+
+/**
+ *
+ * @author 823212965
+ */
+public class Entrega01GUIdeloginsemfuncionalidades {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
